732be84d813014f662e0b0a497bb0bad1df5a4cc
thenatural86 <philip.smalls86@gmail.com>
2020-11-16 16:58:47 -0500

    form variables

---
+++ mail/static/mail/compose.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('hello')
+    const form = document.querySelector('#compose-form');
+    const recipients = document.querySelector('#compose-recipients');
+    const subject = document.querySelector('#compose-subject');
+    const body = document.querySelector('#compose-body');
+})
+
+
